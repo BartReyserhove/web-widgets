5579d2614876d84d1b186e1fe187e5970b6441ae
Rahman <rahmanunver@gmail.com>
2023-10-12 18:30:56 +0200

    fix(combobox-web): a11y tweaks for Clear button, aria-label, focus state

--- packages/pluggableWidgets/combobox-web/src/components/SingleSelection/SingleSelection.tsx
+++ packages/pluggableWidgets/combobox-web/src/components/SingleSelection/SingleSelection.tsx
@@ -50,6 +50,7 @@ export function SingleSelection({
                     <button
                         tabIndex={tabIndex}
                         className="widget-combobox-clear-button"
+                        aria-label="Clear selection"
                         onClick={e => {
                             e.stopPropagation();
                             inputRef.current?.focus();
